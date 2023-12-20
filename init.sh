3673beb80c4b4335673ab5dd906e5a83b1ccbc36
pietrangelo <posta@pietrangelo.net>
2023-12-20 16:41:49 +0100

    bash: add new condition for the `PAUSE` env var
    Added a new condition to verify the value of the `PAUSE` env var and if it's true we check if also `PAUSE_TIME` exists.

--- init.sh
+++ init.sh
@@ -4,6 +4,16 @@ set -xe
 DIRECTORY="/entando-data/public"
 FINAL_PATH="$REPO_PATH$ARCHIVE_NAME"
 
+if "$PAUSE"; then
+  if [ -z "$PAUSE_TIME" ]; then
+    echo "PAUSE_TIME env var must be specified."
+    exit 44
+  fi
+  echo "Sleeping for $PAUSE_TIME"
+  sleep "$PAUSE_TIME"
+fi
+
+
 if [ ! -d "$DIRECTORY" ]; then
     wget --no-check-certificate "$FINAL_PATH" -P /tmp
     tar -xf /tmp/"$ARCHIVE_NAME" -C /
