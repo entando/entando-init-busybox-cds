d9386f4f25c7fc69b9107c269f88ecf754ade784
pietrangelo <p.masala@entando.com>
2024-01-31 15:02:55 +0100

    ENG-5409: Refactor script and improve data handling
    Refactored init.sh script to enhance the handling of data initialization and overwrite processes. Introduced the 'perform_download_and_extraction' function for better code structure and readability. Renamed variable names for clarity, and improved the logic flow for situations when 'FORCE_CDS' or 'SKIP_INIT' is set.

--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 set -xe
 
-DIRECTORY="/entando-data/public"
-FINAL_PATH="$REPO_PATH$ARCHIVE_NAME"
+DATA_DIRECTORY="/entando-data/public"
+ARCHIVE_URL="$REPO_PATH$ARCHIVE_NAME"
+PAUSE_ENABLED="$PAUSE"
 
-if "$PAUSE"; then
+# Performs download and extraction of archive.
+perform_download_and_extraction() {
+  wget --no-check-certificate "$ARCHIVE_URL" -P /tmp
+  tar -xf /tmp/"$ARCHIVE_NAME" -C /
+  rm /tmp/"$ARCHIVE_NAME"
+}
+
+if [ "$PAUSE_ENABLED" = true ]; then
   if [ -z "$PAUSE_TIME" ]; then
     echo "PAUSE_TIME env var must be specified."
     exit 44
@@ -13,17 +21,17 @@ if "$PAUSE"; then
   sleep "$PAUSE_TIME"
 fi
 
+if [ "$FORCE_CDS" = true ]; then
+  perform_download_and_extraction
+  echo "entando-data has been overwritten correctly"
+  exit 0
+fi
 
-if [ ! -d "$DIRECTORY" ]; then
-    wget --no-check-certificate "$FINAL_PATH" -P /tmp
-    tar -xf /tmp/"$ARCHIVE_NAME" -C /
-    rm /tmp/"$ARCHIVE_NAME"
-    echo "entando-data has been initialized correctly"
-elif "$FORCE_CDS"; then
-    wget --no-check-certificate "$FINAL_PATH" -P /tmp
-    tar -xf /tmp/"$ARCHIVE_NAME" -C /
-    rm /tmp/"$ARCHIVE_NAME"
-    echo "entando-data has been overwritten correctly"
+if [ "$SKIP_INIT" = true ]; then
+  echo "data initialization has been skipped"
+elif [ ! -d "$DATA_DIRECTORY" ]; then
+  perform_download_and_extraction
+  echo "entando-data has been initialized correctly"
 else
-    echo "entando-data is already in place..."
+  echo "entando-data is already in place..."
 fi
